b8090b4a1df309621941f2ca48cdda25a96ecd05
Denver Lambson <VZXKCW@NAM.CORP.GM.COM>
2023-09-27 14:47:46 -0700

    framework setup

---
+++ DealerOn Challenge/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealerOn_Challenge
+{
+    public class Item
+    {
+        // Name of the item
+        public string? Name { get; set; }
+
+        // Price with a 2 digit decimal
+        public decimal Price { get; set; }
+
+        // SalesTax with a 2 digit decimal
+        public decimal SalesTax { get; set; }
+
+        // IsImported, boolean
+        public bool IsImported { get; set; }
+
+        // ImportTax with a 2 digit decimal
+        public decimal ImportTax { get; set; }
+
+        // Total with a 2 digit decimal
+        public decimal Total { get; set; }
+    }
+}
--- DealerOn Challenge/Program.cs
+++ DealerOn Challenge/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DealerOn_Challenge;
+using System;
+
+namespace DealerOnChallenge {
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sales Taxes\n____________________________");
+            Console.WriteLine("Please enter items you wish to purchase. When finished, do Ctrl+Z and press 'Enter' to finish:");
+
+            string[] inputs = InputItems();
+            foreach (string line in inputs)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+
+
+        public static string[] InputItems()
+        {
+            string input = Console.In.ReadToEnd();
+            string[] inputs = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("You entered:");
+            return inputs;
+        }
+
+        //parse the input into a list of objects
+        public static List<Item> createItems(string[] items)
+        {
+            List<Item> itemsObj = new List<Item>();
+
+            return itemsObj;
+        }
+    }
+}
+
+
+
+
+//Parse the strings in the array
+
+
+
