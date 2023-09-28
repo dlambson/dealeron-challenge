d056204813ec8213a8156c171bc251cfbb1cf966
Denver Lambson <VZXKCW@NAM.CORP.GM.COM>
2023-09-27 21:40:11 -0700

    finishing logic changes

--- DealerOn Challenge/Program.cs
+++ DealerOn Challenge/Program.cs
@@ -11,7 +11,7 @@ namespace DealerOnChallenge {
     public class Program
     {
         // this will contain terms that will determine if an item is tax exempt or not
-        readonly static string[] exemptTerms = { "book", "chocolate", "bar", "chocolates", "pills" };
+        readonly static string[] exemptTerms = { "book", "chocolate", "chocolates", "pills" };
 
         static void Main(string[] args)
         {
@@ -23,18 +23,7 @@ namespace DealerOnChallenge {
             List<Item> items = CreateItems(inputs);
             items = AssignTaxes(items);
 
-            foreach(var item in items)
-            {
-                Console.WriteLine("Name: " + item.Name);
-                Console.WriteLine("Quantity: " + item.Quantity);
-                Console.WriteLine("Price: " + item.Price);
-                Console.WriteLine("SalesTax: " + item.SalesTax);
-                Console.WriteLine("IsImported: " + item.IsImported);
-                Console.WriteLine("IsTaxExempt: " + item.IsTaxExempt);
-                Console.WriteLine("ImportTax: " + item.ImportTax);
-                Console.WriteLine("Total: " + item.Total);
-                Console.WriteLine("\n\n");
-            }
+            OutputReceipt(items);
         }
 
         // create a string list of all the inputs
@@ -92,8 +81,8 @@ namespace DealerOnChallenge {
         //assign taxes to the object list
         public static List<Item> AssignTaxes(List<Item> items)
         {
-            Console.WriteLine("Hello? what is in the list: " + items.Count());
             int taxPercentage = 10;
+            decimal importTax = 0.05m;
             foreach(var item in items)
             {
                 item.IsTaxExempt = IsTaxExempt(item.Name.ToLower());
@@ -102,14 +91,63 @@ namespace DealerOnChallenge {
                 {
                     item.SalesTax = Math.Ceiling((item.Price / taxPercentage) * 20) / 20;
                 }
+
+                if (item.IsImported)
+                {
+                    item.ImportTax = Math.Ceiling((item.Price * importTax) * 20) / 20;
+                }
+
+                item.Total = item.Price + item.SalesTax + item.ImportTax;
             }
 
             return items;
         }
 
+        //returns if an item is exempt based on terms found in the input
         public static bool IsTaxExempt(string name)
         {
             return exemptTerms.Any(t => name == t || name.Contains(t));
+        }
+
+        //return list of unique items (removes duplicate items if any)
+        public static List<Item> GetUniqueItems(List<Item> items)
+        {
+            return items.GroupBy(item => new { item.Name, item.Price }).Select(group => group.First()).ToList();
+        }
+
+        //takes the list of item objects and totals up the receipts
+        public static void OutputReceipt(List<Item> items)
+        {
+            Console.WriteLine("\n------------Output--------------\n");
+            decimal total = 0.00m;
+            decimal totalSalesTax = 0.00m;
+            string output = "";
+            var uniqueItems = GetUniqueItems(items);
+            
+            foreach(var item in uniqueItems)
+            {
+                //check for duplicate items finding by name and price
+                int duplicates = items.Count(i => i.Name == item.Name && i.Price == item.Price);
+                output += $"{item.Name}: ";
+                //if there are more than one of the same item in the list, then multiply the duplicate totals and add them to the total
+                if (duplicates > 1)
+                {
+                    decimal itemMultTotal = item.Total * duplicates;
+                    output += $"{itemMultTotal} ({duplicates} @ {item.Total})\n";
+                    totalSalesTax += duplicates * (item.SalesTax + item.ImportTax);
+                    total += duplicates * (item.Total);
+                } else
+                {
+                    output += $"{item.Total}\n";
+                    totalSalesTax += item.SalesTax + item.ImportTax;
+                    total += item.Total;
+                }
+            }
+
+            output += $"Sales Taxes: {totalSalesTax}\n";
+            output += $"Total: {total}";
+
+            Console.WriteLine(output);
         }
     }
 }
