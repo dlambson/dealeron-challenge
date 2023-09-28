9c7ad62ed87779e5af3fe242e677082e93ba288f
Denver Lambson <VZXKCW@NAM.CORP.GM.COM>
2023-09-27 19:05:48 -0700

    more added methods

--- DealerOn Challenge/Item.cs
+++ DealerOn Challenge/Item.cs
@@ -8,8 +8,22 @@ namespace DealerOn_Challenge
 {
     public class Item
     {
+        public Item()
+        {
+            Name = null;
+            Quantity = 0;
+            Price = 0.00m;
+            SalesTax = 0.00m;
+            IsImported = false;
+            IsTaxExempt = false;
+            ImportTax = 0.00m;
+            Total = 0.00m;
+        }
+
         // Name of the item
         public string? Name { get; set; }
+
+        public int Quantity { get; set; }
 
         // Price with a 2 digit decimal
         public decimal Price { get; set; }
@@ -19,6 +33,9 @@ namespace DealerOn_Challenge
 
         // IsImported, boolean
         public bool IsImported { get; set; }
+
+        // IsTaxExempt, boolean
+        public bool IsTaxExempt { get; set; }
 
         // ImportTax with a 2 digit decimal
         public decimal ImportTax { get; set; }
--- DealerOn Challenge/Program.cs
+++ DealerOn Challenge/Program.cs
@@ -1,48 +1,115 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DealerOn_Challenge;
 using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Markup;
 
 namespace DealerOnChallenge {
 
-
     public class Program
     {
+        // this will contain terms that will determine if an item is tax exempt or not
+        readonly static string[] exemptTerms = { "book", "chocolate", "bar", "chocolates", "pills" };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Sales Taxes\n____________________________");
-            Console.WriteLine("Please enter items you wish to purchase. When finished, do Ctrl+Z and press 'Enter' to finish:");
+            Console.WriteLine("Please enter items you wish to purchase. Enter a ';' on the last item to finish entering items");
 
-            string[] inputs = InputItems();
-            foreach (string line in inputs)
+            var inputs = InputItems();
+
+            List<Item> items = CreateItems(inputs);
+            items = AssignTaxes(items);
+
+            foreach(var item in items)
             {
-                Console.WriteLine(line);
+                Console.WriteLine("Name: " + item.Name);
+                Console.WriteLine("Quantity: " + item.Quantity);
+                Console.WriteLine("Price: " + item.Price);
+                Console.WriteLine("SalesTax: " + item.SalesTax);
+                Console.WriteLine("IsImported: " + item.IsImported);
+                Console.WriteLine("IsTaxExempt: " + item.IsTaxExempt);
+                Console.WriteLine("ImportTax: " + item.ImportTax);
+                Console.WriteLine("Total: " + item.Total);
+                Console.WriteLine("\n\n");
             }
-
         }
 
-
-        public static string[] InputItems()
+        // create a string list of all the inputs
+        public static List<string> InputItems()
         {
-            string input = Console.In.ReadToEnd();
-            string[] inputs = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine("You entered:");
-            return inputs;
+            var inputs = new List<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.EndsWith(";"))
+                {
+                    inputs.Add(input.TrimEnd(';'));
+                    break;
+                } else
+                {
+                    inputs.Add(input);
+                }
+            }
+            return inputs ;
         }
 
         //parse the input into a list of objects
-        public static List<Item> createItems(string[] items)
+        public static List<Item> CreateItems(List<string> items)
         {
             List<Item> itemsObj = new List<Item>();
 
+            foreach(string item in items)
+            {
+                Item obj = new Item();
+                string pattern = @"(\d+)\s+(.*?)\s+at\s+(\d+\.\d{2})";
+                Match match = Regex.Match(item, pattern);
+                if (match.Success)
+                {   
+                    obj.Quantity = Convert.ToInt16(match.Groups[1].Value);
+                    obj.Name = match.Groups[2].Value;
+                    obj.Price = Convert.ToDecimal(match.Groups[3].Value);
+
+                    if (obj.Name.Contains("Imported")) {
+                        obj.IsImported = true;
+                    }
+
+                    itemsObj.Add(obj);
+
+                } else
+                {
+                    continue;
+                }
+            }
+
             return itemsObj;
+        }
+
+        //assign taxes to the object list
+        public static List<Item> AssignTaxes(List<Item> items)
+        {
+            Console.WriteLine("Hello? what is in the list: " + items.Count());
+            int taxPercentage = 10;
+            foreach(var item in items)
+            {
+                item.IsTaxExempt = IsTaxExempt(item.Name.ToLower());
+
+                if (!item.IsTaxExempt)
+                {
+                    item.SalesTax = Math.Ceiling((item.Price / taxPercentage) * 20) / 20;
+                }
+            }
+
+            return items;
+        }
+
+        public static bool IsTaxExempt(string name)
+        {
+            return exemptTerms.Any(t => name == t || name.Contains(t));
         }
     }
 }
-
-
-
-
-//Parse the strings in the array
-
-
-
